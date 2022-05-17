51301b65adcd104ba96e76a7190fcc99704a911d
Thiago Santos <thiagomtd2@gmail.com>
2022-05-17 12:48:28 -0300

    add image

--- src/components/ItemCard/mock.ts
+++ src/components/ItemCard/mock.ts
@@ -3,13 +3,13 @@ import { ItemCardProps } from '.'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
-    imageSrc: 'https://logosmarcas.net/wp-content/uploads/2021/12/MMs-Logo.png',
+    imageSrc: '/MMs-Logo.png',
     price: 5,
     title: "MM's",
     weight: 25,
   },
   {
-    imageSrc: 'https://dori.com.br/wp-content/uploads/2019/07/logo-DORI.png',
+    imageSrc: '/logo-DORI.png',
     price: 5,
     title: 'Amendoim',
     weight: 25,
