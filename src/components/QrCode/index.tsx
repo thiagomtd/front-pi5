8b2dbc3bd54b7bb399cd2b52f34f3aa79ce59065
Thiago Santos <thiagomtd2@gmail.com>
2022-05-18 19:26:15 -0300

    qrcode

--- src/components/QrCode/index.tsx
+++ src/components/QrCode/index.tsx
@@ -1,12 +1,26 @@
 import * as S from './styles'
+import QRCode from 'react-qr-code'
 
-const QrCode = () => {
+const QrCode = (context: any) => {
+  console.log(context)
   return (
     <S.SelectWrapper>
       <S.QrCodeTitle>Escaneie o QrCode para realizar o pagamento</S.QrCodeTitle>
-      <S.QrCodeImage src='https://www.gov.br/inss/pt-br/centrais-de-conteudo/imagens/qr-code-novo-fw-300x300-png' />
+      <QRCode value='00020101021226880014BR.GOV.BCB.PIX2566qrcodes-pix.gerencianet.com.br/v2/bf264e36b6784162b5b2da73b4f519825204000053039865802BR5914GERENCIANET SA6010OURO PRETO62070503***6304DFB7' />
     </S.SelectWrapper>
   )
 }
 
 export default QrCode
+
+//127.0.0.1:5000/orders
+
+// export const getServerSideProps = async () => {
+//   return {
+//     props: { hello: 'oi' },
+//   }
+// }
+
+export async function getServerSideProps(context: any) {
+  return { props: { data: 'aqui' } }
+}
